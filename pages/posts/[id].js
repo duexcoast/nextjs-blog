7564a0c3014afb034148f4cfb2f804a712489776
uxconor <conor.ux@gmail.com>
2022-06-17 13:42:47 -0400

    added links to blog posts and styling

--- pages/posts/[id].js
+++ pages/posts/[id].js
@@ -1,17 +1,24 @@
+import Head from "next/head";
 import React from "react";
 import Layout from "../../components/layout";
 import { getAllPostIds, getPostData } from "../../lib/posts";
+import Date from "../../components/date";
+import utilStyles from "../../styles/utils.module.css";
 
 export default function Post({ postData }) {
   return (
     <Layout>
-      {postData.title}
-      <br />
-      {postData.id}
-      <br />
-      {postData.date}
-      <br />
-      <div dangerouslySetInnerHTML={{ __html: postData.contentHTML }} />
+      <Head>
+        <title>{postData.title}</title>
+      </Head>
+      <article>
+        <h1 className={utilStyles.headingXL}>{postData.title}</h1>
+
+        <div className={utilStyles.lightText}>
+          <Date dateString={postData.date} />
+        </div>
+        <div dangerouslySetInnerHTML={{ __html: postData.contentHTML }} />
+      </article>
     </Layout>
   );
 }
