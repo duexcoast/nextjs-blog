37d3a6befb0c80cb4b2015f96be68847947ee236
uxconor <conor.ux@gmail.com>
2022-06-20 14:19:55 -0400

    fixed static images

--- components/layout.js
+++ components/layout.js
@@ -3,7 +3,6 @@ import Image from "next/image";
 import styles from "./layout.module.css";
 import utilStyles from "../styles/utils.module.css";
 import Link from "next/link";
-import profilePic from "../public/images/profile.jpg";
 
 const name = "Conor Ney";
 export const siteTitle = "Next.js Sample Website";
@@ -31,7 +30,7 @@ export default function Layout({ children, home }) {
           <>
             <Image
               priority
-              src={profilePic}
+              src="/images/profile.jpg"
               className={utilStyles.borderCircle}
               height={144}
               width={144}
@@ -45,7 +44,7 @@ export default function Layout({ children, home }) {
               <a>
                 <Image
                   priority
-                  src={profilePic}
+                  src="/images/profile.jpg"
                   className={utilStyles.borderCircle}
                   height={108}
                   width={108}
